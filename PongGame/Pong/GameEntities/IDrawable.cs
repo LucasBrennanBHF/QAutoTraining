fc2a11f71403bb0d46272933a0d779ab74060e65
Lucas Brennan <brennanl@bhf.org.uk>
2024-05-16 12:43:54 +0100

    Coffee shop

---
+++ PongGame/Pong/GameEntities/IDrawable.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Pong.GameEntities
+{
+    public interface IDrawable
+    {
+        public int XPos { get; }
+        public int YPos { get; }
+        public Rectangle GetHitBox();
+    }
+}
